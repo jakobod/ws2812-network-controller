09709e795f2ba260adb364776e794b27b9615344
Jakob Otto <jakob.otto@haw-hamburg.de>
2019-07-28 18:41:39 +0200

    Add answer to broadcast messages from App

--- kitchen_lamp.ino
+++ kitchen_lamp.ino
@@ -8,9 +8,13 @@
 WiFiUDP udp;
 uint16_t localPort = 23;
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; //buffer to hold incoming packet,
+char replyBuffer[] = "acknowledged\r\n";       // a string to send back
 
 const uint16_t PixelCount = 66; // this example assumes 3 pixels, making it smaller will cause a failure
 NeoPixelBrightnessBus<NeoRgbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount, 0);
+RgbColor currentColor(0,0,0);
+
+void updateLeds();
 
 void setup() {
   Serial.begin(115200);
@@ -34,17 +38,30 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   udp.begin(localPort);
+
+  currentColor = RgbColor(0,255,0);
+  updateLeds();
+}
+
+void updateLeds() {
+  for (int i = 0; i < PixelCount; ++i) {
+    strip.SetPixelColor(i, currentColor);
+  }
+  strip.Show();
 }
 
 void parsePacket(const String& msg) { 
-  String r = msg.substring(0,3);
-  String g = msg.substring(3,6);
-  String b = msg.substring(6,9);
-
-  for (int i = 0; i < PixelCount; ++i) {
-    strip.SetPixelColor(i, RgbColor(g.toInt(), r.toInt(), b.toInt()));
+  if (msg == "/LAMPS_GATHER_AND_ANSWER") {
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    udp.write(replyBuffer);
+    udp.endPacket();
+  } else {
+    String r = msg.substring(0,3);
+    String g = msg.substring(3,6);
+    String b = msg.substring(6,9);
+  
+    currentColor = RgbColor(g.toInt(), r.toInt(), b.toInt());
   }
-  strip.Show();
 }
 
 void loop() {
@@ -56,4 +73,6 @@ void loop() {
     packetBuffer[n] = 0;
     parsePacket(packetBuffer);
   }
+  
+  updateLeds();
 }
